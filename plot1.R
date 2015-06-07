200b29646f7180cc62933a69e5439ae2005bfc11
larusion <larusion@users.noreply.github.com>
2015-06-07 16:00:28 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,11 +1,18 @@
+
+# First we read the data
 DATA <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?", stringsAsFactors = FALSE)
+
+# We paste date and time and then convert the class 
 datetime <- paste(DATA$Date, DATA$Time)
 datetime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 Project <- cbind(datetime, DATA)
+
+# We extract the part of the data we are going analyze
 keywords <- strptime(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
 KEY <- paste(keywords, collapse = "|")
 Project2 <- Project[grepl(KEY, Project$datetime),]
 
+# We plot plot1.png
 png(file = "plot1.png", width = 480, height = 480)
 
 with(Project2, hist(Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)"))
