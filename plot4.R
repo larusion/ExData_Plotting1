c6c8a3cf1afc3bd051e0998046e1446bb656b168
larusion <larusion@users.noreply.github.com>
2015-06-07 16:53:21 +0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,17 +1,29 @@
+
+# First we read the data
 DATA <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?", stringsAsFactors = FALSE)
+
+
+# We paste date and time creating a new variable called datetime and then convert its class using strptime()
 datetime <- paste(DATA$Date, DATA$Time)
 datetime <- strptime(datetime, "%d/%m/%Y %H:%M:%S")
 Project <- cbind(datetime, DATA)
+
+
+# We extract the part of the data we are going to analyze
 keywords <- strptime(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
 KEY <- paste(keywords, collapse = "|")
 Project2 <- Project[grepl(KEY, Project$datetime),]
 
+
+
+# We construct the plot and save it to a png file
 png(file = "plot4.png", width = 480, height = 480)
 
 par(mfrow = c(2,2))
 
 with(Project2, plot(datetime, Global_active_power, type = "l", xlab = "", ylab = "Global Active Power"))
 with(Project2, lines(datetime, Global_active_power))
+
 
 with(Project2, plot(datetime, Voltage, type = "l"))
 
@@ -23,7 +35,5 @@ legend("topright", lty = c(1, 1, 1), col = c("black", "red", "blue"), legend = c
 
 
 with(Project2, plot(datetime, Global_reactive_power, type = "l"))
-
-
 
 dev.off()
